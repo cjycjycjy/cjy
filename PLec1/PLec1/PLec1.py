1a412bce967ce7d908c5ec375ba4120c772d0f7c
cjycjycjy <cjy12120@gmail.com>
2015-09-02 13:47:36 +0900

    lec1
    문자열 예제

---
+++ PLec1/PLec1/PLec1.py
@@ -0,0 +1,42 @@
+﻿#print('i have {0:8d} second {1:4d}'.format(6,7))
+#print('%03d'%6)
+
+
+#salary = int(input("enter salary\n"))
+#bonus = int(input("enter bonus\n"))
+#paycheck = salary+ bonus
+#print(paycheck)
+
+#tmp = float(salary)+float(bonus)
+#print(tmp)
+
+#data = 15.151515
+#print(round(data,2))
+#print("ㅁㄴㅇ")
+#print(type(paycheck))
+
+
+#print("asdasdasd\n")
+#name = "abcdefg"
+#print(name[0])
+#print(name[-1])
+#print(name[-2])
+#print(name[-7])
+#print(name[:5])
+
+
+#a = "asd %s"%"five"
+#print(a)
+
+#b= "abcd"
+#print(b)
+#b = b[:2]+'ccc'+b[2:]
+#print(b)
+#print("asdffadf %d%%"%3)
+
+
+tmp = input("종료할까요?????   ")
+upper = tmp.upper()
+lower = tmp.lower()
+print(upper)
+print(lower)
