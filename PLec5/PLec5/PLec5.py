b46e0d4ad57cad6b2151e1e4ba82285467ff0e38
cjycjycjy <cjy12120@gmail.com>
2015-09-16 13:53:23 +0900

    5번째 실습
    파일 다루기

---
+++ PLec5/PLec5/PLec5.py
@@ -0,0 +1,30 @@
+﻿import pickle
+fileName = "test.txt"
+fileName2 = "monica.txt"
+
+with open(fileName,"r") as myFile, open(fileName2,"wb") as monica :
+    #myFile.write("201011301 최종윤\n")
+    #myFile.write("201011302 최종윤\n")
+    #myFile.write("201011303 최종윤\n")
+    #myFile.write("201011304 최종윤\n")
+    #myFile.write("201011305 최종윤\n")
+    #while True:
+    #    content = myFile.readline()
+    #    if not content: break
+    #    print(content)
+    data = []
+    index = 0
+    for content in myFile:
+        (role,etc) = content.strip().split(":",1)  #strip() 공백 제거 (인자 하나 추가해서 횟수 제한)
+        #if role == "Monica":
+            #monica.write(etc)
+            #monica.write("\n")
+        data.append(role)
+                
+    
+    pickle.dump(data,monica)
+
+with open(fileName2,"rb") as monica :
+     result = pickle.load(monica)
+     print(result)
+    
