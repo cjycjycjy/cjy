01569280ae30db6e79b418a5b8e09d8f4b92e53f
cjycjycjy <cjy12120@gmail.com>
2015-11-30 13:47:42 +0900

    행렬
    행렬연산

--- PLec19/PLec19/PLec19.py
+++ PLec19/PLec19/PLec19.py
@@ -1,4 +1,4 @@
-import numpy as np
+﻿import numpy as np
 from matplotlib import pyplot as plt
 data = np.array([[1,2,3],[4,5,6],[7,8,9]])
 #print(data)
@@ -41,12 +41,158 @@ data = np.array([[1,2,3],[4,5,6],[7,8,9]])
 #b = y>20
 #print(y[b])
 
-d = np.arange(36).reshape(6,6)
-print(d)
-mask = np.array(np.array([1,0,1,0,0,1],dtype = bool))
-print(d[mask,3])
-#mask1=np.array([3,4,5,3,4,5,3,4,5])
-#mask2=np.array([0,0,0,2,2,2,5,5,5])
-mask1=np.array([3,4,5])
-mask2=np.array([0,2,5])
-print(d[3: ,np.array([0,2,5])])
+#d = np.arange(36).reshape(6,6)
+#print(d)
+#mask = np.array(np.array([1,0,1,0,0,1],dtype = bool))
+#print(d[mask,3])
+##mask1=np.array([3,4,5,3,4,5,3,4,5])
+##mask2=np.array([0,0,0,2,2,2,5,5,5])
+#mask1=np.array([3,4,5])
+#mask2=np.array([0,2,5])
+#print(d[3: ,np.array([0,2,5])])
+
+##값이 두번 들어간다 곱하기 2하면 - list
+#temp = [1,2,3,4]
+#print(temp*2)
+
+##행렬로 만들면 각 원소의 값이 두배로 커진다 곱하기 2하면 - 행렬
+#print(data*2)
+#print(data*data)
+#print(data+2)
+
+##행렬의 곱셈
+#print(data.dot(data))
+
+a = np.array([1,2,3,4])
+b = np.array([4,2,2,4])
+c = np.array([1,2,3,4])
+
+##각 원소간의 비교
+#print(a==b)
+#print(a>b)
+
+
+##행렬간에 같은지 다른지 비교
+#print(np.array_equal(a,c))
+
+#d = np.array([1,0,1,0], dtype = bool)
+#e = np.array([1,1,0,0], dtype = bool)
+#print(np.logical_or(d,e))
+#print(np.logical_and(d,e))
+
+##안의 리스트에 전부다 True여야 True 반환
+#print(np.all([True,True,False]))
+#any 는 하나라도 True면 True 반환
+#print(np.any([True,True,False]))
+
+## 초월 함수
+#f = np.arange(5)
+#print(np.sin(f))
+#print(np.cos(f))
+#print(np.exp(f))
+
+##전이행렬
+#t = np.triu(np.ones((3,3), dtype = int),1) #상삼각행렬
+#print(t)
+#print(t.T)
+
+#print(np.sum(a))
+#print(a.sum())
+
+#x = np.array([[1,1],[2,2]])
+#print(x)
+#print(x.sum(axis=0)) # columns
+#print(x.sum(axis=1)) # rows
+
+#print(x[0,:].sum())
+#print(x[1,:].sum())
+
+#print(data)
+#print(data.min())
+#print(data.max())
+#print(data.argmax())
+#print(data.argmin())
+
+#print(data.mean())# 원소들의 평균
+#print(np.median(data))#원소들의 중간값
+#print(np.median(data,axis=0))#축설정
+
+#print(data.std())
+##############################################################
+#data = np.loadtxt('data.txt')
+##print(data)
+
+#year,hares,lynxes,carrots = data.T
+#print(data)
+#print(data.mean(axis=0))
+#print(data.std(axis=0))
+#print(year[hares.argmax()])
+#print(year[lynxes.argmax()])
+#print(year[carrots.argmax()])
+#print(np.mean(hares))
+#print(np.mean(lynxes))
+#print(np.mean(carrots))
+#print(np.std(hares))
+#print(np.std(lynxes))
+#print(np.std(carrots))
+
+
+################################################################
+#print(data.T) 
+#plt.plot(year,hares,year,lynxes,year,carrots)
+#plt.show()
+
+
+a =np.tile(np.arange(0,60,10),(6,1)).T
+print(a)
+b = np.array([0,1,2,3,4,5])
+data = a+b
+print(data)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
