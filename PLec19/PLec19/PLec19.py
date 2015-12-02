7aa65cce84d14d8ba3e354b2f8a08253d9920169
cjycjycjy <cjy12120@gmail.com>
2015-12-02 13:41:48 +0900

    numpy
    numpy

--- PLec19/PLec19/PLec19.py
+++ PLec19/PLec19/PLec19.py
@@ -143,30 +143,115 @@ c = np.array([1,2,3,4])
 #plt.show()
 
 
-a =np.tile(np.arange(0,60,10),(6,1)).T
-print(a)
-b = np.array([0,1,2,3,4,5])
-data = a+b
-print(data)
+#a =np.tile(np.arange(0,60,10),(6,1)).T
+#print(a)
+#b = np.array([0,1,2,3,4,5])
+#data = a+b
+#print(data)
 
+#12/2 - Broadcasting 예
 
+#mileposts = np.array([0, 198, 303, 736, 871, 1175, 1475, 1544, 1913, 2448])#print(mileposts[:, np.newaxis])#distance_array = np.abs(mileposts - mileposts[:, np.newaxis])
+#print(distance_array)
 
+####
 
+#x,y = np.arange(5), np.arange(5)[:,np.newaxis]
+#distance = np.sqrt(x**2+y**2)
+#print(distance)
 
+#plt.pcolor(distance)
+#plt.colorbar()
+#plt.show()
 
+####
 
+#x,y = np.mgrid[0:5,0:5]
+#x,y = np.mgrid[0:5,0:5]
+#print(x)
+#print(y)
 
+#x,y = np.ogrid[0:5,0:5]
+#print(x+y)
 
+####
+#a = np.array([[1,2,3],[4,5,6]])
+#print(a.ravel())
+#print(a.T)
+#print(a.T.ravel())
 
+#b = a.ravel()
+#print(b)
+#b = b.reshape((2,3))
+#print(b)
 
+####
 
+#z =np.array([1,2,3])
+#print(z)
+#print(z[:,np.newaxis])
+#print(z[np.newaxis,:])
 
+#a = np.arange(4)
+#print(a)
+#a.resize(8)
+#print(a)
 
+####
 
+#a =  np.array([[4,3,5,6],[1,7,2,5]])
+#b = np.sort(a,axis=1)
+#print(a)
+#print(b)
+#a.sort(axis=0)
+#print(a)
 
+#a = np.array([4,3,1,2])
+#print(a)
+#j = np.argsort(a)
+#print(j)
+#j_max = np.argmax(a)
+#print(j_max)
+#j_min = np.argmin(a)
+#print(j_min)
 
+#####
 
+#p = np.poly1d([3,2,-1])
+#print(p(0))
+#print(p.roots)
+#print(p.order)
+#p = np.polynomial.Polynomial([-1,2,3])
+#print(p(-1))
 
+########
+
+#x = np.linspace(0,1,20)
+#y = np.cos(x)+ 0.3*np.random.rand(20)
+#p = np.poly1d(np.polyfit(x,y,3))
+#print(p)
+#t = np.linspace(0, 1, 200)
+#plt.plot(x, y, 'y*', t, p(t), 'k*')
+#plt.show()
+##############
+
+#x = np.linspace(-1, 1, 2000)
+#y = np.cos(x) + 0.3*np.random.rand(2000)
+#p = np.polynomial.Chebyshev.fit(x, y, 90)
+#t = np.linspace(-1, 1, 200)
+#plt.plot(x, y, 'r.')
+#plt.plot(t, p(t), 'k-', lw=3)
+#plt.show()
+
+############
+
+img = plt.imread('image.png')
+print(img.shape,img.dtype)
+
+temp = img[50:100,:100]  #일부 이미지 가져올때 슬라이싱
+
+plt.imshow(temp)
+plt.show()
 
 
 
